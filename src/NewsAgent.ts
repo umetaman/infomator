5c70e38468d00995a8ab8790006e0d76740b27d0
umetaman <umeta@w0w.co.jp>
2020-06-08 00:36:17 +0900

    ニュースの情報を取得して、表示する

--- src/NewsAgent.ts
+++ src/NewsAgent.ts
@@ -1,3 +1,12 @@
+export interface NewsHeadline{
+    Title: string;
+    Link: string;
+}
+
+export interface HeadlineCallback{
+    (headlines: NewsHeadline[]): void;
+}
+
 export class NewsAgent{
 
     private request: XMLHttpRequest;
@@ -6,5 +15,40 @@ export class NewsAgent{
         this.request = new XMLHttpRequest();
     }
 
-    
+    public GetHeadlines(onComplete: HeadlineCallback): void{
+        this.request.open("GET", "https://news.google.com/news/rss/headlines/section/topic/TECHNOLOGY?hl=ja&gl=JP&ceid=JP:ja");
+        
+        this.request.onreadystatechange = () => {
+            if(this.request.readyState != 4){
+                // requesting
+            }else if(this.request.status != 200){
+                // failed
+                console.log("request is failed.");
+            }
+            else{
+                const response = this.request.response;
+                const parser: DOMParser = new DOMParser();
+
+                const doms = parser.parseFromString(response, "application/xml");
+                
+                // parse XML to headline interfaces...
+                const root = doms.getElementsByTagName("channel")[0];
+                const items = root.getElementsByTagName("item");
+                
+                let headlines: NewsHeadline[] = [];
+                for(let i = 0; i < items.length; i++){
+                    let headline: NewsHeadline = {
+                        Title: items[i].getElementsByTagName("title")[0].innerHTML,
+                        Link: items[i].getElementsByTagName("link")[0].innerHTML
+                    };
+                    
+                    headlines.push(headline);
+                }
+
+                onComplete(headlines);
+            }
+        }
+
+        this.request.send(null);
+    }
 }
--- src/bundle.ts
+++ src/bundle.ts
@@ -1,3 +1,5 @@
+import { NewsAgent } from "./NewsAgent"
+
 export class RenderMain{
 
     // date
@@ -16,10 +18,10 @@ export class RenderMain{
     private newsDescription: HTMLElement | null;
 
     // http request for Google news
-    private request: XMLHttpRequest;
+    private newsHost: NewsAgent;
 
     constructor(){
-        this.request = new XMLHttpRequest();
+        this.newsHost = new NewsAgent();
     }
 
     public SetupDOMs(): void{
@@ -34,8 +36,6 @@ export class RenderMain{
         this.newsTitle = document.getElementById("news_title");
         this.newsCategory = document.getElementById("news_category");
         this.newsDescription = document.getElementById("news_description");
-    
-        this.setNewsContent("", [], "");
     }
 
     public StartClock(): void{
@@ -54,33 +54,9 @@ export class RenderMain{
         this.second?.innerText = ( "00" + date.getSeconds()).slice(-2);
     }
 
-    private setNewsContent(title: string, category: string[], description: string): void{
-        this.newsTitle?.innerText = "Unity2019 LTS　リリース開始。";
-        this.newsDescription?.innerText = "Unityテクノロジーズは、本日、同社が開発するゲームエンジン「Unity」の2019LTSのリリースを発表。";
-    }
-
-    public GetNewsHeadLines(): void{
-        this.request.open("GET", "https://news.google.com/news/rss/headlines/section/topic/TECHNOLOGY?hl=ja&gl=JP&ceid=JP:ja");
-        this.request.onreadystatechange = () =>{
-            if(this.request.readyState != 4){
-                // requesting
-            }else if(this.request.status != 200){
-                // failed
-                console.log("request is failed.");
-            }
-            else{
-                const response = this.request.response;
-                const parser: DOMParser = new DOMParser();
-
-                const doms = parser.parseFromString(response, "application/xml");
-                const titles = doms.documentElement.getElementsByTagName("title");
-                for(let i = 0; i < titles.length; i++){
-                    console.log(titles[i].textContent);
-                }
-            }
-        }
-
-        this.request.send(null);
+    public SetNewsContent(title: string, category: string[], description: string): void{
+        this.newsTitle?.innerText = title;
+        this.newsDescription?.innerText = description;
     }
 }
 
@@ -88,5 +64,11 @@ window.onload = () => {
     const renderMain: RenderMain = new RenderMain();
     renderMain.SetupDOMs();
     renderMain.StartClock();
-    renderMain.GetNewsHeadLines();
+
+    const newsAgent: NewsAgent = new NewsAgent();
+    newsAgent.GetHeadlines((headlines) => {
+        const first = headlines[0];
+
+        renderMain.SetNewsContent(first.Title, [], first.Title);
+    });
 };
